b4c13d2cd63984f8d0ad455607f48a052164a9dc
chibimiku <dreamcosine@gmail.com>
2015-12-23 19:08:08 +0800

    update enterence

--- pdnovel.php
+++ pdnovel.php
@@ -7,8 +7,27 @@
 /*                   */
 /*********************/
 
+foreach($_POST as $p_key => $check_p){
+	if(stripos($check_p, '\'') !== false){
+		//exit('access_denied...');
+		$_POST[$p_key] = str_replace('\'', '/', $check_p);
+	}
+}
+
 define( 'APPTYPEID', 11 );
 define( 'CURSCRIPT', 'pdnovel' );
+
+$modarray = array( 'index', 'list', 'cat', 'ajax', 'view', 'misc', 'chapter', 'read', 'download', 'comment', 'novelcp', 'home', 'search', 'topic' , 'batch' , 'author');
+if(!isset($_GET['mod']) || !$_GET['mod']){
+	$_GET['mod'] = 'index';
+}
+if(!in_array($_GET['mod'], $modarray)){
+	exit('mod error.');
+}
+if ( empty( $_GET['mod'] ) || !in_array( $_GET['mod'],  $modarray) )
+{
+		$GLOBALS['_GET']['mod'] = 'cat';
+}
 require( './source/class/class_core.php' );
 require( './source/discuz_version.php' );
 $discuz =& discuz_core::instance( );
@@ -23,10 +42,6 @@ if ( !empty( $_G['setting']['domain']['app']['pdnovel'] ) && $_SERVER['HTTP_HOST
 $version = substr( DISCUZ_VERSION , 0 , 4 );
 @include( DISCUZ_ROOT.'./source/language/'.CURSCRIPT.'/lang_template.php' );
 require( DISCUZ_ROOT.'./source/function/function_'.CURSCRIPT.'.php' );
-if ( empty( $_GET['mod'] ) || !in_array( $_GET['mod'], array( 'index', 'list', 'cat', 'ajax', 'view', 'misc', 'chapter', 'read', 'download', 'comment', 'novelcp', 'home', 'search', 'topic' , 'batch') ) )
-{
-		$GLOBALS['_GET']['mod'] = 'cat';
-}
 define( 'CURMODULE', $_GET['mod'] );
 runhooks( );
 $pdmodule = DB::fetch_first( 'SELECT * FROM '.DB::table( 'pdmodule_view' ).' WHERE moduleid='.APPTYPEID );
