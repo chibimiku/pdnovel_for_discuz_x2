6673089d79015b668988d1f171bac1a9340bbae3
chibimiku <dreamcosine@gmail.com>
2015-12-24 11:34:26 +0800

    update language chaoscode issue

--- source/language/lang_block_pdnovel.php
+++ source/language/lang_block_pdnovel.php
@@ -1,3 +1,42 @@
-<?php // This file is protected by copyright law & provided under license. Copyright(C) 2012-2020 www.poudu.net,www.chnb.net, All rights reserved.
-$OOO0O0O00=__FILE__;$OOO000000=urldecode('%74%68%36%73%62%65%68%71%6c%61%34%63%6f%5f%73%61%64%66%70%6e%72');$OO00O0000=2336;$OOO0000O0=$OOO000000{4}.$OOO000000{9}.$OOO000000{3}.$OOO000000{5};$OOO0000O0.=$OOO000000{2}.$OOO000000{10}.$OOO000000{13}.$OOO000000{16};$OOO0000O0.=$OOO0000O0{3}.$OOO000000{11}.$OOO000000{12}.$OOO0000O0{7}.$OOO000000{5};$O0O0000O0='OOO0000O0';eval(($$O0O0000O0('JE9PME9PMDAwMD0kT09PMDAwMDAwezE3fS4kT09PMDAwMDAwezEyfS4kT09PMDAwMDAwezE4fS4kT09PMDAwMDAwezV9LiRPT08wMDAwMDB7MTl9O2lmKCEwKSRPMDAwTzBPMDA9JE9PME9PMDAwMCgkT09PME8wTzAwLCdyYicpOyRPTzBPTzAwME89JE9PTzAwMDAwMHsxN30uJE9PTzAwMDAwMHsyMH0uJE9PTzAwMDAwMHs1fS4kT09PMDAwMDAwezl9LiRPT08wMDAwMDB7MTZ9OyRPTzBPTzAwTzA9JE9PTzAwMDAwMHsxNH0uJE9PTzAwMDAwMHswfS4kT09PMDAwMDAwezIwfS4kT09PMDAwMDAwezB9LiRPT08wMDAwMDB7MjB9OyRPTzBPTzAwME8oJE8wMDBPME8wMCwxMjc0KTskT08wME8wME8wPSgkT09PMDAwME8wKCRPTzBPTzAwTzAoJE9PME9PMDAwTygkTzAwME8wTzAwLDM4MCksJ0VudGVyeW91d2toUkhZS05XT1VUQWFCYkNjRGRGZkdnSWlKakxsTW1QcFFxU3NWdlh4WnowMTIzNDU2Nzg5Ky89JywnQUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkrLycpKSk7ZXZhbCgkT08wME8wME8wKTs=')));return;?>
-kr9NHenNHenNHe1zfukgFMaXdoyjcUImb19oUAxyb18mRtwmwJ4LT09NHr8XTzEXRJwmwJXPkr9NTzEXHenNHtILT08XT08XHr8XhtONTznNTzEXHr8Pkr8XHenNHr8XHtXLT08XHr8XHeEXhUXmOB50cbk5d3a3D2iUUylRTlfNaaOnCAkJW2YrcrcMO2fkDApQToxYdanXAbyTF1c2BuiDGjExHjH0YTC3KeLqRz0mRtfnWLYrOAcuUrlhU0xYTL9WAakTayaBa1icBMyJC2OlcMfPDBpqdo1Vd3nxFmY0fbc3Gul6HerZHzW1YjF4KUSvkZLphUL7cMYSd3YlhtONHeEXTznNHeEpK2a2CBXPkr9NHenNHenNHtL7eWPLdoyVcZE9woyZFMy5eWPPeWPmCMxvC2sjdoyzF19Xco5vfMaSkz0+k9tiZ7bE4tFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb3nLdM92cBXmNT4m0huRsFUjv+LmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy91FMXmNT4m0huRsFo0vfHmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy90DbOScUF9NJgWPFV1SGqH4JFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb2cpcBxLb3npCZF9NJgWPFV1s+Re5JFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb2cpcBxLb3Y1dB1iFmLmNT4m0huRsdzZvGLmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy9LCbOldolVcUF9NJG3Pqh8Zqo85tFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb2cpcBxLb2y1foivFMlLkz0+k9g31f9aUAWmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy9Vd3cldolLkz0+k9tiZ7akOtFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb2cpcBxLb2y1foivFJF9NJgb99bgX/SmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy9ifbOPd3k1FMXmNT4m1/ga38o0vfHmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy9ifMy0CbwmNT4m08K7p823z/rmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy9ifMy0CbkgdBlLcoxlkz0+k9NeV6gYs8/xhYdWhUFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb2cpcBxLb2y2CbOiFl9JDBFmNT4m08K7p823z/rPsNHpkZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgC2y0fbkSkz0+k7d+vRD31SeIXdU90ZFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb2cpcBxLb2Yifo5idBAmNT4msv68sqgBXKee+7NokZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgfbn1FMXmNT4m0qV8sqgBXKensR3TkZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgfbnVCB1lkz0+k9h7vRD31SeIX/VzxJFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb2cpcBxLb2xiF3OjDoyXfoaZkz0+k9gV0HRaXq3DkZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgdoyzfoYPCbn0cbk1FMXmNT4m1+7WXsbtvfqnsR3TkZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgcoy5fMlzDbWmNT4mZYB147v3kZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgf2alD3cpF2l0kz0+k9dFsGK79ZFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb2cpcBxLb21vdmOPfMlzDbWmNT4m1Hh147v3kZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgCBxSfMlzDbWmNT4m19Z147v3kZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgcoy5fM90cUF9NJgw1F3ovNCmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy93cBaqfM90cUF9NJgB3H3ovNCmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy9sd250DucvfoAmNT4m1HRYxqz2kZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgCBxSfM90cUF9NJgb3H3ovNCmRE0hk2kSd2YqC2xiF3YgFoOVd3cldy9MDBaScy9idoxsCbkqkz0+k9gFZsBZ2tFSeWPmCMxvC2sjdoyzF19Xco5vfMaSb2cpcBxLb3fvFMOzkz0+k9gF19dh/Fq9kZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgcMlldoOgC29sdBaVfuHmNT4mxSet28q9kZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgF2YZDbn0b25vfMaSdMa3kz0+k9gV0HRWPFV1kZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgF2YZDbn0b25vfMaSDo90kz0+k8jwX8bWPFV1kZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgF2YZDbn0b25vfMaSF3nlC2lMDBaLkz0+k9D4sQjWPFV1kZXYtJfJdo9jD2YSCbYzb3nLdM92cBxgF2YZDbn0b25vfMaSkz0+k7jgvRdb1RDP0VAmRE0hhTSYtI==
+<?php
+
+$lang = array
+(
+	'blockclass_pdnovel' => '小说类',
+	'blockclass_pdnovel_pdnovel' => '小说模块',
+	'blockclass_pdnovel_field_url' => '小说链接',
+	'blockclass_pdnovel_field_title' => '小说标题',
+	'blockclass_pdnovel_field_pic' => '小说封面',
+	'blockclass_pdnovel_field_summary' => '小说简介',
+	'blockclass_pdnovel_field_dateline' => '发布时间',
+	'blockclass_pdnovel_field_authorid' => '作者UID',
+	'blockclass_pdnovel_field_novelid' => '小说ID',
+	'blockclass_pdnovel_field_author' => '作者名',
+	'blockclass_pdnovel_field_authorurl' => '作者链接',
+	'blockclass_pdnovel_field_avatar' => '用户头像',
+	'blockclass_pdnovel_field_avatar_middle' => '用户头像(中)',
+	'blockclass_pdnovel_field_avatar_big' => '用户头像(大)',
+	'blockclass_pdnovel_field_caturl' => '二级分类链接',
+	'blockclass_pdnovel_field_catname' => '二级分类名称',
+	'blockclass_pdnovel_field_upurl' => '一级分类链接',
+	'blockclass_pdnovel_field_upname' => '一级分类名称',
+	'blockclass_pdnovel_field_lastchapter' => '最新章节',
+	'blockclass_pdnovel_field_lastchapterurl' => '最新章节链接',
+	'blockclass_pdnovel_field_dayvisit' => '日点击',
+	'blockclass_pdnovel_field_weekvisit' => '周点击',
+	'blockclass_pdnovel_field_monthvisit' => '月点击',
+	'blockclass_pdnovel_field_allvisit' => '总点击',
+	'blockclass_pdnovel_field_dayvote' => '日推荐',
+	'blockclass_pdnovel_field_weekvote' => '周推荐',
+	'blockclass_pdnovel_field_monthvote' => '月推荐',
+	'blockclass_pdnovel_field_allvote' => '总推荐',
+	'blockclass_pdnovel_field_allmark' => '总收藏',
+	'blockclass_pdnovel_field_words' => '总字数数',
+	'blockclass_pdnovel_field_comments' => '评论数',
+	'blockclass_pdnovel_script_novelnew' => '最新小说',
+	'blockclass_pdnovel_script_novelhot' => '热门小说',
+	'blockclass_pdnovel_script_novelspecified' => '指定小说',
+	'blockclass_pdnovel_script_novel' => '高级自定义',
+);
+
+?>
